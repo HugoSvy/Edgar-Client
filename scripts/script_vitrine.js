10017ea16e53530beb5b5061b83d8f6d9f498172
usrlocal <usrlocal@GA319-23-08.ad.univ-angers.fr>
2024-12-06 11:12:03 +0100

    vitrine et organisation

---
+++ scripts/script_vitrine.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+        duration: 1000, // durée de l'animation en millisecondes
+        once: true // si `true`, l'animation s'exécute une seule fois
+    });
+});
